ae1d6b102317aac4675b4bf59687ebf6950331bb
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-12 15:35:34 +0100

    Init commit

---
+++ src/main/java/ru/vaspoz/relo/WhereToLiveApplication.java
@@ -0,0 +1,12 @@
+package ru.vaspoz.relo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereToLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereToLiveApplication.class, args);
+	}
+}
