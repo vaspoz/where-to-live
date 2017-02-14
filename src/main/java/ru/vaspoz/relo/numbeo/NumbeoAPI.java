c2daf9a838307c3f813ff055ad81f908b24fb5cc
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-14 14:58:07 +0100

    Get list of expenses from html table

--- src/main/java/ru/vaspoz/relo/numbeo/NumbeoAPI.java
+++ src/main/java/ru/vaspoz/relo/numbeo/NumbeoAPI.java
@@ -2,10 +2,16 @@ package ru.vaspoz.relo.numbeo;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import ru.vaspoz.relo.numbeo.model.OverallRates;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringJoiner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class NumbeoAPI {
 
@@ -24,13 +30,19 @@ public class NumbeoAPI {
         Document doc;
         try{
             doc = Jsoup.connect(getLink(baseCountry, baseCity, compareToCountry, compareToCity)).get();
-            String salaryDIf = doc.select("tr:contains(Average Monthly Disposable Salary)>td")
+            String salaryDif = doc.select("tr:contains(Average Monthly Disposable Salary)>td")
                     .last()         //returns columns. Percentage locates in last column
                     .children()     //returns list of tags in last column
                     .last()         //desired tag
                     .childNode(0).toString().replace("%", "");      //get innerText of view " + 24.5% ". And make it = " + 24.5% "
 
-            System.out.println(doc);
+            // Get all percentages except salary. It will produce array of expenses
+            Elements columnElements = doc.select("tr:not(:contains(Average Monthly Disposable Salary))>td>span");
+            List<String> expensesDif = Arrays.stream(columnElements.toArray())
+                    .map(elt -> ((Element)elt).text())
+                    .filter(elt -> elt.contains("%"))
+                    .map(elt -> elt.replace("%", "")).collect(Collectors.toList());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
