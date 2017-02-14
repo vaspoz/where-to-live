e9d1ddbb2e119eb059c09827ee761c30e8d3c90e
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-14 17:43:52 +0100

    Exception for showing problem with result link itself

---
+++ src/main/java/ru/vaspoz/relo/exceptions/ParsingDoublesException.java
@@ -0,0 +1,13 @@
+package ru.vaspoz.relo.exceptions;
+
+public class ParsingDoublesException extends Exception {
+
+    public ParsingDoublesException() {
+        super();
+    }
+
+    public ParsingDoublesException(String message) {
+        super(message);
+    }
+
+}
