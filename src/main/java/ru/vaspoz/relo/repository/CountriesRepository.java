0410482fb66351496936ad59f915a81fad396098
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-14 20:08:15 +0100

    Find by countryName

--- src/main/java/ru/vaspoz/relo/repository/CountriesRepository.java
+++ src/main/java/ru/vaspoz/relo/repository/CountriesRepository.java
@@ -6,4 +6,7 @@ import ru.vaspoz.relo.model.Country;
 
 @Repository
 public interface CountriesRepository extends CrudRepository<Country, Integer> {
+
+    public Country findByCountry(String countryName);
+
 }
