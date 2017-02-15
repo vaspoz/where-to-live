6db7d03dfe8733ed0da80d7736adfe1fd581927c
Vasilii_Pozdeev <vaspoz@mail.ru>
2017-02-15 18:44:40 +0100

    Inject response header to every response

---
+++ src/main/java/ru/vaspoz/relo/ResponseFilter.java
@@ -0,0 +1,24 @@
+package ru.vaspoz.relo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+
+@Component
+public class ResponseFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper(httpServletResponse);
+        wrapper.addHeader("Access-Control-Allow-Origin", "*");
+
+        filterChain.doFilter(httpServletRequest, wrapper);
+    }
+}
