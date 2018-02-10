217cca402e690edcda7387e138d8bda195569a67
vaspoz <vaspoz@mail.ru>
2018-02-10 17:10:44 +0100

    Tomcat-ready

--- src/main/java/ru/vaspoz/relo/WhereToLiveApplication.java
+++ src/main/java/ru/vaspoz/relo/WhereToLiveApplication.java
@@ -2,9 +2,16 @@ package ru.vaspoz.relo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WhereToLiveApplication {
+public class WhereToLiveApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WhereToLiveApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhereToLiveApplication.class, args);
