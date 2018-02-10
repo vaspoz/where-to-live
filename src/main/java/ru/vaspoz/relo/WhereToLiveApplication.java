a984c193e44a848d4e26f1008eb56330c8961703
vaspoz <vaspoz@mail.ru>
2018-02-10 16:25:08 +0100

    Rollback

--- src/main/java/ru/vaspoz/relo/WhereToLiveApplication.java
+++ src/main/java/ru/vaspoz/relo/WhereToLiveApplication.java
@@ -2,16 +2,9 @@ package ru.vaspoz.relo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WhereToLiveApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WhereToLiveApplication.class);
-	}
+public class WhereToLiveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhereToLiveApplication.class, args);
