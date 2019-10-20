779ed2d9abe79bdc221f3ec64799dede3ce10c9f
vaspoz <vaspoz@mail.ru>
2019-10-20 17:56:21 +0200

    new db table to keep user creds

---
+++ src/main/resources/db/migration/V4__create_user_login_table.sql
@@ -0,0 +1,7 @@
+create table users (
+    id                  int primary key auto_increment,
+    username            varchar,
+    password            varchar,
+    email               varchar,
+    country_origin      varchar
+);
