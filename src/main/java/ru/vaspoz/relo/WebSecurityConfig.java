85c43316688a3ed038546d944c50901ee77cbe05
vaspoz <vaspoz@mail.ru>
2020-02-07 17:17:26 +0100

    Test prod deployment

--- src/main/java/ru/vaspoz/relo/WebSecurityConfig.java
+++ src/main/java/ru/vaspoz/relo/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/login", "/signup", "/h2/**", "/ping").permitAll()
+                .authorizeRequests().antMatchers("/login", "/signup", "/h2/**", "/ping", "/countries").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 // all other requests need to be authenticated
                 .anyRequest().authenticated().and()
