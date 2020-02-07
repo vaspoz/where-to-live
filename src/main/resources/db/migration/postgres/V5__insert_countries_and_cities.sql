bdd4e3ccb1451f3ba7ab11c6835c6430ff973d33
vaspoz <vaspoz@mail.ru>
2020-02-07 18:18:55 +0100

    prod uri changed

--- src/main/resources/db/migration/postgres/V5__insert_countries_and_cities.sql
+++ src/main/resources/db/migration/postgres/V5__insert_countries_and_cities.sql
@@ -1,2 +1,2 @@
-COPY COUNTRIES FROM 'countries.csv' DELIMITER ',' CSV HEADER;
-COPY CITIES FROM 'cities.csv' DELIMITER ',' CSV HEADER;
+COPY COUNTRIES FROM 'classpath:db/migration/countries.csv' DELIMITER ',' CSV HEADER;
+COPY CITIES FROM 'classpath:db/migration/cities.csv' DELIMITER ',' CSV HEADER;
