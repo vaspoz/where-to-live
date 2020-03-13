7eb5f3a1b4b4cc723d9ba014e2bf08b5d26b5491
vaspoz <vaspoz@mail.ru>
2020-03-13 23:10:38 +0100

    [fix] Hibernate sequence - IDENTITY

--- src/main/java/ru/vaspoz/relo/model/AliyahLog.java
+++ src/main/java/ru/vaspoz/relo/model/AliyahLog.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class AliyahLog {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String username;
